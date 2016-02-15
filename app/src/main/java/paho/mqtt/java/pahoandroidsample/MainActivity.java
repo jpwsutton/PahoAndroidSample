1359ac1053f9b4e18013666641c047be9fae479a
James Sutton <james.sutton@uk.ibm.com>
2016-02-15 13:02:14 +0000

    Initial commit of basic example
    Signed-off-by: James Sutton <james.sutton@uk.ibm.com>

---
+++ app/src/main/java/paho/mqtt/java/pahoandroidsample/MainActivity.java
@@ -0,0 +1,68 @@
+package paho.mqtt.java.pahoandroidsample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import org.eclipse.paho.android.service.MqttAndroidClient;
+import org.eclipse.paho.client.mqttv3.IMqttActionListener;
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.IMqttToken;
+import org.eclipse.paho.client.mqttv3.MqttCallback;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final MqttAndroidClient mqttAndroidClient = new MqttAndroidClient(this.getApplicationContext(), "tcp://iot.eclipse.org:1883", "androidSampleClient");
+        mqttAndroidClient.setCallback(new MqttCallback() {
+            @Override
+            public void connectionLost(Throwable cause) {
+                System.out.println("Connection was lost!");
+
+            }
+
+            @Override
+            public void messageArrived(String topic, MqttMessage message) throws Exception {
+                System.out.println("Message Arrived!: " + topic + ": " + new String(message.getPayload()));
+
+            }
+
+            @Override
+            public void deliveryComplete(IMqttDeliveryToken token) {
+                System.out.println("Delivery Complete!");
+            }
+        });
+
+        try {
+            mqttAndroidClient.connect(null, new IMqttActionListener() {
+                @Override
+                public void onSuccess(IMqttToken asyncActionToken) {
+                    System.out.println("Connection Success!");
+                    try {
+                        System.out.println("Subscribing to /test");
+                        mqttAndroidClient.subscribe("/test", 0);
+                        System.out.println("Subscribed to /test");
+                        System.out.println("Publishing message..");
+                        mqttAndroidClient.publish("/test", new MqttMessage("Hello world!".getBytes()));
+                    } catch (MqttException ex) {
+
+                    }
+
+                }
+
+                @Override
+                public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+                    System.out.println("Connection Failure!");
+                }
+            });
+        } catch (MqttException ex) {
+
+        }
+
+
+    }
+}
